8bf8b9edce98fbaa362a805ae288df1401b72cd8
yilJesus009 <giljesusgilpantoja@gmail.com>
2026-02-26 02:05:20 -0400

    actualizando conecion ./db

--- PROYECTO_FINAL/backend/src/db.js
+++ PROYECTO_FINAL/backend/src/db.js
@@ -1,14 +1,12 @@
 require("dotenv").config();
-const pool = require("pg");
+const { Pool } = require("pg");
 
-const pool = new Pool({
+const conexionBD = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: Number(process.env.DB_PORT || 5501),
-  waitForConnections: true,
-  connectionLimit: 10,
 });
 
-module.exports = pool;
+module.exports = conexionBD;
